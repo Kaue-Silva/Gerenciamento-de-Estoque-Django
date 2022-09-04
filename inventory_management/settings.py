e52da235926e791cbfed92af922a0b11321aa815
KaueSil <Kaue-Silva>
2022-09-04 20:11:13 -0300

    templates forms

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third Party Apps
+    "crispy_forms",
     # Local Apps
     "users",
 ]
