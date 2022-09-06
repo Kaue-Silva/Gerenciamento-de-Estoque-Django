f72e4a151be2c565b82478c07bd6c949f35bffaf
KaueSil <Kaue-Silva>
2022-09-06 20:18:04 -0300

    rename urls users

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("user/", include("users.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("users/", include("users.urls"))]
